be922381973f09984e44d070ed7e79f4bb4a03bb
Giga <gigabyte5671@gmail.com>
2021-09-24 23:58:13 +1200

    Update dom-particles.js
    Created particle object reference.

--- dom-particles.js
+++ dom-particles.js
@@ -1,3 +1,17 @@
+var particle = {
+  creation_tick: 0,
+  start: [0,0],
+  end: [0,0],
+  duration: 0,
+  lifespan: 0,
+  appearance_function: {
+    scale: [1,1],
+    scale: [1,1]
+  }
+}
+
+
+
 function doParticles(){
   
   
