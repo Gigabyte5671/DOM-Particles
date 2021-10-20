7c1fe9bb637229efba025138bd7c83ad76843941
NotNatural21 <89242396+NotNatural21@users.noreply.github.com>
2021-10-21 11:53:09 +1300

    Update dom-particles.js

--- dom-particles.js
+++ dom-particles.js
@@ -30,7 +30,22 @@ function doParticles(TS){
       if(tick > p.creation_tick + p.lifespan){
         particles.splice(i,1);
       }
-      
+
+      if (start === undefined){
+        start = timestamp;
+      }
+      const elapsed = timestamp - start;
+
+      if (previousTimeStamp !== timestamp) {
+        // Math.min() is used here to make sure the element stops at exactly 200px
+        const count = Math.min(0.1 * elapsed, 200);
+        element.style.transform = 'translateX(' + count + 'px)';
+      }
+
+      if (elapsed < 2000) { // Stop the animation after 2 seconds
+        previousTimeStamp = timestamp
+        window.requestAnimationFrame(step);
+      } 
     });
   }
   
